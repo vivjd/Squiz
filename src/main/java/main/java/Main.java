34809e8b7f4dedc93b4d981068bba6a88abdd9a8
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-10-23 17:38:39 -0400

    rename object

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -2,21 +2,22 @@ package main.java;
 
 import main.java.data_access.NoteDataAccessObject;
 import main.java.entity.Note;
+import main.java.usecase.note.NoteDataAccessInterface;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // Example usage of the NoteDataAccessObject
-        NoteDataAccessObject dataAccess = new NoteDataAccessObject();
+        NoteDataAccessInterface noteDataAccess = new NoteDataAccessObject();
         Note note = new Note();
         note.setTitle("Example Title");
         note.setUserPrompt("Example Prompt");
 
-        dataAccess.saveNote(note);
+        noteDataAccess.saveNote(note);
 
         // Retrieve all notes
-        List<String> allNotes = dataAccess.getAllNotes();
+        List<String> allNotes = noteDataAccess.getAllNotes();
         for (String noteJson : allNotes) {
             System.out.println(noteJson);
         }
