568b9c9a0275721a045932aa44255c096f758c53
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-10-23 17:39:02 -0400

    comment out placeholder

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -10,11 +10,11 @@ public class Main {
     public static void main(String[] args) {
         // Example usage of the NoteDataAccessObject
         NoteDataAccessInterface noteDataAccess = new NoteDataAccessObject();
-        Note note = new Note();
-        note.setTitle("Example Title");
-        note.setUserPrompt("Example Prompt");
-
-        noteDataAccess.saveNote(note);
+//        Note note = new Note();
+//        note.setTitle("Example Title");
+//        note.setUserPrompt("Example Prompt");
+//
+//        noteDataAccess.saveNote(note);
 
         // Retrieve all notes
         List<String> allNotes = noteDataAccess.getAllNotes();
