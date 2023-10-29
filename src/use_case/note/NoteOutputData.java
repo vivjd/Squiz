67dc5750d947645a87db0d160188aca6e8433366
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-10-29 01:58:15 -0400

    Done

--- src/use_case/note/NoteOutputData.java
+++ src/use_case/note/NoteOutputData.java
@@ -1,4 +1,27 @@
 package use_case.note;
 
 public class NoteOutputData {
+
+    private final String title;
+    private Note note;
+
+    private boolean useCaseFailed;
+
+    public SignupOutputData(String title, Note note, boolean useCaseFailed) {
+        this.title = title;
+        this.note = note;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getNote() {
+        return this.note;
+    }
+
+    public void setNote(Note newNote) {
+        this.note = newNote;
+    }
 }
