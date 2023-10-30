e229d375b040867aca3db4d0434fd92246cc1593
emmshen <emmy12821@gmail.com>
2023-10-30 17:17:56 -0400

    edited NoteView

--- src/view/NoteView.java
+++ src/view/NoteView.java
@@ -1,6 +1,9 @@
 package view;
 
 
+import interface_adapter.note.NoteController;
+import interface_adapter.note.NoteViewModel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,6 +16,22 @@ import java.beans.PropertyChangeListener;
 public class NoteView extends JPanel implements ActionListener, PropertyChangeListener{
     public final String viewName = "note";
 
+    private final JTextField userInputNote = new JFormattedTextField(30);
+    private final JButton save;
+    private final JButton edit;
+    private final JButton generate_quiz;
+
+
+    private final NoteController noteController;
+    private final NoteViewModel noteViewModel;
+
+    public NoteView(NoteController noteController, NoteViewModel noteViewModel) {
+        this.noteController = noteController;
+        this.noteViewModel = noteViewModel;
+        noteViewModel.addPropertyChangeListener(this);
+
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
