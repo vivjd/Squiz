705e7493031228124c487b63952a8fb9bd28ca34
Vivian <118199397+vivjd@users.noreply.github.com>
2023-10-30 20:31:03 -0400

    Added Javadoc for NoteOutputData.java

--- src/use_case/note/NoteOutputData.java
+++ src/use_case/note/NoteOutputData.java
@@ -1,12 +1,23 @@
 package use_case.note;
 
+/**
+ * The `NoteOutputData` class represents the output data from the Save Notes use case.
+ * It contains information about the note's title and associated input text, as well as a flag
+ * indicating whether the save notes operation failed.
+ */
 public class NoteOutputData {
-
     private final String title;
     private String inputText;
 
     private boolean useCaseFailed;
 
+    /**
+     * Constructs a `NoteOutputData` object with the provided title, input text, and use case failure status.
+     *
+     * @param title The title of the note.
+     * @param inputText The input text associated with the note.
+     * @param useCaseFailed A boolean indicating whether the save notes use case failed.
+     */
     public NoteOutputData(String title, String inputText, boolean useCaseFailed) {
         this.title = title;
         this.inputText = inputText;
@@ -20,5 +31,8 @@ public class NoteOutputData {
     public String getInputText() {
         return this.inputText;
     }
-    public void setInputText(String inputText){this.inputText = inputText;}
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
+    }
 }
