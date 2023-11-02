92a2ad2a82bbceba81c773791a276f3cd9d8db77
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-02 19:38:02 -0400

    Some getters and setters that are relevent to our implementation are implemented

--- src/entity/Quiz.java
+++ src/entity/Quiz.java
@@ -8,5 +8,18 @@ public class Quiz {
     int quizLength = 0;
     int numCorrect = 0;
 
+    public Quiz(){
+    }
+
+    public int getQuizLength() {
+        return quizLength;
+    }
+    public int getNumCorrect() {
+        return numCorrect;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
 
 }
