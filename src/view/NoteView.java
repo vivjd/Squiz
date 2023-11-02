ae91155c2e6e5fbe0c6fcc0e01f237341fe9adca
emmshen <emmy12821@gmail.com>
2023-11-01 22:05:31 -0400

    Finished the NoteView constructor and added propertyChange method

--- src/view/NoteView.java
+++ src/view/NoteView.java
@@ -2,6 +2,7 @@ package view;
 
 
 import interface_adapter.note.NoteController;
+import interface_adapter.note.NoteState;
 import interface_adapter.note.NoteViewModel;
 
 import javax.swing.*;
@@ -16,7 +17,7 @@ import java.beans.PropertyChangeListener;
 public class NoteView extends JPanel implements ActionListener, PropertyChangeListener{
     public final String viewName = "note";
 
-    private final JTextField userInputNote = new JFormattedTextField(30);
+    private final JTextField userInputNote = new JTextField("enter your notes here");
     private final JButton save;
     private final JButton edit;
     private final JButton generate_quiz;
@@ -30,7 +31,62 @@ public class NoteView extends JPanel implements ActionListener, PropertyChangeLi
         this.noteViewModel = noteViewModel;
         noteViewModel.addPropertyChangeListener(this);
 
+        JLabel title = new JLabel(NoteViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        LabelTextPanel noteInfo = new LabelTextPanel(
+                new JLabel(NoteViewModel.NOTE_LABEL), userInputNote);
+        userInputNote.setSize(50,50);
+
+        JPanel buttons = new JPanel();
+        edit = new JButton(NoteViewModel.EDIT_LABEL);
+        save = new JButton(NoteViewModel.SAVE_LABEL);
+        generate_quiz = new JButton(NoteViewModel.SUBMIT_BUTTON_LABEL);
+        buttons.add(edit);
+        buttons.add(save);
+        buttons.add(generate_quiz);
+
+        save.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(save)) {
+                            NoteState currentState = noteViewModel.getState();
+
+                            noteController.execute(
+                                    currentState.getTitle(),
+                                    currentState.getNote());
+                            showSavedPopup();
+                        }
+                    }
+                }
+        );
+
+        userInputNote.addKeyListener(
+                new KeyListener() {
+
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        NoteState currentState = noteViewModel.getState();
+                        String text = userInputNote.getText() + e.getKeyChar();
+                        currentState.setNote(text);
+                        noteViewModel.setState(currentState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+                    }
+                }
+        );
+
+    }
+
+    private void showSavedPopup(){
+        JOptionPane.showMessageDialog(this, "Note Saved.");
     }
 
     @Override
@@ -40,6 +96,9 @@ public class NoteView extends JPanel implements ActionListener, PropertyChangeLi
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+            NoteState state = (NoteState) evt.getNewValue();
+            if (state.getEmptyNoteError() != null) {
+                JOptionPane.showMessageDialog(this, state.getEmptyNoteError());
+            }
     }
 }
