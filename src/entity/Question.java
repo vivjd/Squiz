7bcfd915bdd20799d86256e76087c7a4260e1df8
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-02 11:30:38 -0400

    Question abstract class revised

--- src/entity/Question.java
+++ src/entity/Question.java
@@ -4,21 +4,10 @@ public abstract class Question {
 
     boolean answerDisplayed = false;
     String question;
-    String answer;
 
-    public void displayAnswer(){
-        this.answerDisplayed = true
-    }
-    public void setQuestion(){
-
-    }
-    public String getQuesiton(){
-        return this.question
-    }
-    public void setAnswer(){
-
-    }
-    public String getAnswer(){
-        return this.answer
-    }
+    public void displayAnswer(){}
+    public void setQuestion(){}
+    public String getQuesiton(){}
+    public void setAnswer(){}
+    public String getAnswer(){}
 }
