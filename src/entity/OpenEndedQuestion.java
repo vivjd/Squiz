24c0ed17c268298737bacad2eb5d24d75724909c
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-02 11:33:28 -0400

    OpenEndedQuestion Entity created

--- src/entity/OpenEndedQuestion.java
+++ src/entity/OpenEndedQuestion.java
@@ -1,4 +1,16 @@
 package entity;
 class OpenEndedQuestion extends Question {
-
+    String answer;
+    public void setQuesion(String answer){
+        this.setQuesion(answer);
+    }
+    public String getQuesion(){
+        this.getQuesion();
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
