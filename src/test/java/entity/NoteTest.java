6ec8a4432cfa03819042ae9e2f84e57ff3c9792e
Vivian <118199397+vivjd@users.noreply.github.com>
2023-11-01 20:10:10 -0400

    Created NoteTest.java

---
+++ src/test/java/entity/NoteTest.java
@@ -0,0 +1,14 @@
+package entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
