9eb445714f6cc508a174d611d421aa9bc5d96dfe
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-03 16:57:31 -0400

    Errors fixed -- Main runs fine

--- src/entity/Question.java
+++ src/entity/Question.java
@@ -4,10 +4,15 @@ public abstract class Question {
 
     boolean answerDisplayed = false;
     String question;
+    String correctAnswer;
 
     public void displayAnswer(){}
-    public void setQuestion(){}
-    public String getQuesiton(){}
+    public void setQuestion(String question){}
+    public String getQuestion(){
+        return this.question;
+    }
     public void setCorrectAnswer(){}
-    public String getCorrectAnswer(){}
+    public String getCorrectAnswer(){
+        return this.correctAnswer;
+    }
 }
