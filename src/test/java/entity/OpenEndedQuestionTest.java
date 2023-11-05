dbc667f240b0d279df719bda443f12d3868d356e
Vivian <118199397+vivjd@users.noreply.github.com>
2023-11-04 22:20:19 -0400

    OpenEndedQuestionTest.java test cases

---
+++ src/test/java/entity/OpenEndedQuestionTest.java
@@ -0,0 +1,37 @@
+package entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenEndedQuestionTest {
+    private OpenEndedQuestion sampleQuestion;
+
+    @BeforeEach
+    void setUp() {
+        sampleQuestion = new OpenEndedQuestion("What is Clean Architecture?", "Clean Architecture is a software design pattern.");
+    }
+
+    @Test
+    void displayAnswerTest() {
+//        sampleQuestion.displayAnswer();
+//        assertTrue(sampleQuestion.isAnswerDisplayed());
+    }
+
+    @Test
+    void getQuestionTest() {
+        assertEquals("What is Clean Architecture?", sampleQuestion.getQuestion());
+    }
+
+    @Test
+    void getCorrectAnswerTest() {
+        assertEquals("Clean Architecture is a software design pattern.", sampleQuestion.getCorrectAnswer());
+    }
+
+    @Test
+    void setCorrectAnswerTest() {
+        sampleQuestion.setCorrectAnswer("Software design.");
+        assertEquals("Software design.", sampleQuestion.getCorrectAnswer());
+    }
+}
