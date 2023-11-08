ba61786bea4f8f8d3f7c13cf71d92b8e40455e00
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-11-07 19:58:38 -0500

    delete note presenter

--- src/app/NoteUseCaseFactory.java
+++ src/app/NoteUseCaseFactory.java
@@ -1,17 +1,20 @@
 package app;
 
+import data_access.NoteDataAccessObject;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.note.SaveNoteController;
-import interface_adapter.note.SaveSaveNotePresenter;
-import interface_adapter.note.NoteViewModel;
+import interface_adapter.note.*;
 import use_case.note.NoteDataAccessInterface;
+import use_case.note.delete.DeleteNoteInputBoundary;
+import use_case.note.delete.DeleteNoteInteractor;
+import use_case.note.delete.DeleteNoteOutputBoundary;
 import use_case.note.save.SaveNoteInputBoundary;
 import use_case.note.save.SaveNoteInteractor;
 import use_case.note.save.SaveNoteOutputBoundary;
 import view.NoteView;
 
 public class NoteUseCaseFactory {
-    private NoteUseCaseFactory() {}
+    private NoteUseCaseFactory() {
+    }
 
     public static NoteView create(
             ViewManagerModel viewManagerModel,
@@ -31,6 +34,16 @@ public class NoteUseCaseFactory {
         SaveNoteInputBoundary noteInteractor = new SaveNoteInteractor(noteDataAccessObject, saveNoteOutputBoundary);
 
         return new SaveNoteController(noteInteractor);
+    }
+
+    private static DeleteNoteController deleteNoteUseCase(
+            ViewManagerModel viewManagerModel,
+            NoteViewModel noteViewModel,
+            NoteDataAccessObject noteDataAccessObject) {
+        DeleteNoteOutputBoundary deleteNoteOutputBoundary = new DeleteNotePresenter(noteViewModel, viewManagerModel);
+        DeleteNoteInputBoundary deleteNoteInteractor = new DeleteNoteInteractor(noteDataAccessObject, deleteNoteOutputBoundary);
+
+        return new DeleteNoteController(deleteNoteInteractor);
     }
 
 }
