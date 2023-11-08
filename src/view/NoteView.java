f0cc34989204353fdb6624c1ae807491306c9f38
emmshen <emmy12821@gmail.com>
2023-11-08 11:31:47 -0500

    deleted unwanted import statements

--- src/view/NoteView.java
+++ src/view/NoteView.java
@@ -1,12 +1,10 @@
 package view;
 
-
 import interface_adapter.note.NoteController;
 import interface_adapter.note.NoteState;
 import interface_adapter.note.NoteViewModel;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
