3b8e3ed9dff4ec200547cc35cb3b4a59ed046eea
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-11-18 14:47:57 -0500

    take quiz controller

--- src/interface_adapter/quiz/take_quiz/TakeQuizController.java
+++ src/interface_adapter/quiz/take_quiz/TakeQuizController.java
@@ -1,2 +1,22 @@
-package interface_adapter.quiz.take_quiz;public class TakeQuizController {
+package interface_adapter.quiz.take_quiz;
+
+import org.bson.types.ObjectId;
+import use_case.quiz.take_quiz.TakeQuizInputBoundary;
+import use_case.quiz.take_quiz.TakeQuizInputData;
+
+import java.util.List;
+
+public class TakeQuizController {
+
+    final TakeQuizInputBoundary takeQuizInteractor;
+
+
+    public TakeQuizController(TakeQuizInputBoundary takeQuizInteractor) {
+        this.takeQuizInteractor = takeQuizInteractor;
+    }
+
+    public void execute(String title, ObjectId objectId, List<?> responses){
+        TakeQuizInputData takeQuizInputData = new TakeQuizInputData(title, objectId, responses);
+        takeQuizInteractor.execute(takeQuizInputData);
+    }
 }
