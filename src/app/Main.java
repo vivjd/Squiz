4d823586e283ef18b8fea66ff6e0e6d8577169f8
emmshen <emmy12821@gmail.com>
2023-11-18 22:39:35 -0500

    added method to view DisplayQuizzes screen (to be deleted once we are able to link it after notes page)

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,14 @@
 package app;
 
 import data_access.NoteDataAccessObject;
+import data_access.QuizDataAccessObject;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.note.NoteViewModel;
+
+import interface_adapter.quiz.DisplayQuizzesController;
+import interface_adapter.quiz.DisplayQuizzesViewModel;
+import view.DisplayQuizzesView;
+
 import use_case.note.NoteDataAccessInterface;
 import view.NoteView;
 import view.ViewManager;
@@ -13,7 +19,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        // showQuizzesPage();
         buildInitialPage();
     }
 
@@ -30,9 +36,13 @@ public class Main {
 
         //unsure if this is the right DataAccessObject...
         NoteDataAccessObject noteDataAccessObject = new NoteDataAccessObject();
+        QuizDataAccessObject quizDataAccessObject = new QuizDataAccessObject();
 
         NoteViewModel noteViewModel = new NoteViewModel();
-        NoteView noteView = NoteUseCaseFactory.create(viewManagerModel, noteViewModel, noteDataAccessObject);
+        DisplayQuizzesViewModel displayQuizzesViewModel = new DisplayQuizzesViewModel();
+
+        NoteView noteView = NoteUseCaseFactory.create(viewManagerModel, noteViewModel, noteDataAccessObject,
+                displayQuizzesViewModel, quizDataAccessObject);
         views.add(noteView, noteView.viewName);
 
         viewManagerModel.setActiveView(noteView.viewName);
@@ -41,5 +51,26 @@ public class Main {
         application.pack();
         application.setVisible(true);
 
+    }
+
+    // TODO: Delete after UseCase is Done
+    public static void showQuizzesPage() {
+        JFrame application = new JFrame("test");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+        DisplayQuizzesView quizzesView = new DisplayQuizzesView(new DisplayQuizzesViewModel(), new DisplayQuizzesController());
+
+        views.add(quizzesView, quizzesView.viewName);
+        viewManagerModel.setActiveView(quizzesView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
     }
 }
