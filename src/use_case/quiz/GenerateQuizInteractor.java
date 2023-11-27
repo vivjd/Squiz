1bae37faca47521efdf9a0403052aa4b0bda151b
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-27 10:01:33 -0500

    Chat GPT Prompt modified -- Specified the returned format of multiple choice questions

--- src/use_case/quiz/GenerateQuizInteractor.java
+++ src/use_case/quiz/GenerateQuizInteractor.java
@@ -12,7 +12,8 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
         try {
             String userInput = "create 5 multiple choice questions with 4 options and answer indicated followed by 5 Open-ended questions with answers to each question, each about this note: "
                     + generateQuizInputData.getNote()
-                    + "; for the multiple choice, it should follow the format of question, followed by a hashmap of answer options, which are followed by the index of the correct answer in that hashmap.";
+                    + "; for the multiple choice, it should follow the format of question, followed by a hashmap of answer options, which are followed by the index of the correct answer in that hashmap." +
+                    "Format the answer choices fro multiple choice questions into a hashmap";
             String response = Chatbot.getChatGPTResponse(userInput);
             System.out.println(response);
             GenerateQuizOutputData generateQuizOutputData = new GenerateQuizOutputData(generateQuizInputData.getTitle());
