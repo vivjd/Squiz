b385bb042dcd64e3b736b0d9bdd48e3a77d767e8
emmshen <emmy12821@gmail.com>
2023-11-27 18:42:45 -0500

    added output and input interfaces for back use case (going to the main page)

---
+++ src/use_case/back/BackInputBoundary.java
@@ -0,0 +1,8 @@
+package use_case.back;
+
+public interface BackInputBoundary {
+    /**
+     * Executes the "Display Notes" use case, presenting all the notes to the user.
+     */
+    void execute();
+}
