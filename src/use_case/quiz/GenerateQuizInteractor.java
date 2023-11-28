285af0a38e18d0b1822bc792e3d52704013edf8f
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-28 12:54:53 -0500

    Interactor implemented assuming that the datatypes of all of the attributes in MCQ entity class is changed into String.

--- src/use_case/quiz/GenerateQuizInteractor.java
+++ src/use_case/quiz/GenerateQuizInteractor.java
@@ -1,7 +1,13 @@
 package use_case.quiz;
 
+import data_access.QuizDataAccessObject;
 import entity.MultipleChoiceQuestion;
+import entity.OpenEndedQuestion;
+import entity.Quiz;
+import entity.Question;
+import java.time.LocalDateTime;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,13 +54,65 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
             int opq4q = opq3q + 2;
             int opq5q = opq4q + 2;
 
+            MultipleChoiceQuestion mcq1 = new MultipleChoiceQuestion(entirelist.get(mcq1q),
+                    entirelist.get(mcq1q+1),
+                    entirelist.get(mcq1q+2));
+            MultipleChoiceQuestion mcq2 = new MultipleChoiceQuestion(entirelist.get(mcq2q),
+                    entirelist.get(mcq2q+1),
+                    entirelist.get(mcq1q+2));
+            MultipleChoiceQuestion mcq3 = new MultipleChoiceQuestion(entirelist.get(mcq3q),
+                    entirelist.get(mcq3q+1),
+                    entirelist.get(mcq1q+2));
+            MultipleChoiceQuestion mcq4 = new MultipleChoiceQuestion(entirelist.get(mcq4q),
+                    entirelist.get(mcq4q+1),
+                    entirelist.get(mcq1q+2));
+            MultipleChoiceQuestion mcq5 = new MultipleChoiceQuestion(entirelist.get(mcq5q),
+                    entirelist.get(mcq5q+1),
+                    entirelist.get(mcq1q+2));
+
+            OpenEndedQuestion opq1 = new OpenEndedQuestion(entirelist.get(opq1q), entirelist.get(opq1q+1));
+            OpenEndedQuestion opq2 = new OpenEndedQuestion(entirelist.get(opq2q), entirelist.get(opq2q+1));
+            OpenEndedQuestion opq3 = new OpenEndedQuestion(entirelist.get(opq3q), entirelist.get(opq3q+1));
+            OpenEndedQuestion opq4 = new OpenEndedQuestion(entirelist.get(opq4q), entirelist.get(opq4q+1));
+            OpenEndedQuestion opq5 = new OpenEndedQuestion(entirelist.get(opq5q), entirelist.get(opq5q+1));
+            List<Question<?>> qList = new ArrayList<>();
+
+            qList.add(mcq1);
+            qList.add(mcq2);
+            qList.add(mcq3);
+            qList.add(mcq4);
+            qList.add(mcq5);
+
+            qList.add(opq1);
+            qList.add(opq2);
+            qList.add(opq3);
+            qList.add(opq4);
+            qList.add(opq5);
+
+            Quiz quiz = new Quiz(generateQuizInputData.getTitle(), LocalDateTime.now());
+            quiz.setQuestions(qList);
+            QuizDataAccessObject quizDAO = new QuizDataAccessObject();
+            quizDAO.saveQuiz(quiz);
+
+
 
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
     }
+//    private static int convert(String c){
+//        if (c.equalsIgnoreCase("A")){
+//            return 0;
+//        } else if(c.equalsIgnoreCase("B")){
+//            return 1;
+//        } else if(c.equalsIgnoreCase("C")){
+//            return 2;
+//        }else if(c.equalsIgnoreCase("D")){
+//            return 3;
+//        }
+//        else{
+//            return -1;
+//        }
+//    }
 }
