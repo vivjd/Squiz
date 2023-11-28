0a123df33dba99ad535eb9d485dc4eaed7d40e0f
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-28 13:02:12 -0500

    minor change in the ChatGPT prompt - typo corrected

--- src/use_case/quiz/GenerateQuizInteractor.java
+++ src/use_case/quiz/GenerateQuizInteractor.java
@@ -24,7 +24,7 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
             String userInput = "create 5 multiple choice questions with 4 options and answer indicated followed by 5 Open-ended questions with answers to each question, each about this note: "
                     + generateQuizInputData.getNote()
                     + "; for the multiple choice, it should follow the format of question, followed by a hashmap of answer options, which are followed by the index of the correct answer in that hashmap." +
-                    "Format the answer choices fro multiple choice questions into a hashmap encapsulated in curly braces in one line. Show the correct answer in the next line.";
+                    "Format the answer choices for multiple choice questions into a hashmap encapsulated in curly braces in one line. Show the correct answer choice in the next line.";
             String response = Chatbot.getChatGPTResponse(userInput);
             System.out.println(response);
             GenerateQuizOutputData generateQuizOutputData = new GenerateQuizOutputData(generateQuizInputData.getTitle());
