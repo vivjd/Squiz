489763735acd289696990fd2387b0368fda3dc1b
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-29 20:52:16 -0500

    comments added for interacter to enhence readability.

--- src/use_case/quiz/GenerateQuizInteractor.java
+++ src/use_case/quiz/GenerateQuizInteractor.java
@@ -21,17 +21,31 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
     @Override
     public void execute(GenerateQuizInputData generateQuizInputData) throws Exception {
         try {
+
+            // User's input, note, is obtained through controller and passed in as a part of tailored ChatGPT prompt to
+            // generate the raw string format of quiz.
+
             String userInput = "create 5 multiple choice questions with 4 options and answer indicated followed by 5 Open-ended questions with answers to each question, each about this note: "
                     + generateQuizInputData.getNote()
                     + "; for the multiple choice, it should follow the format of question, followed by a hashmap of answer options, which are followed by the index of the correct answer in that hashmap." +
                     "Format the answer choices for multiple choice questions into a hashmap encapsulated in curly braces in one line. Show the correct answer choice in the next line.";
             String response = Chatbot.getChatGPTResponse(userInput);
             System.out.println(response);
+
+            // To format the raw string appropriately to facilitate converting to various entities, tje raw string is
+            // splitted into an array of String, splitted at every new line escape character. Then the array is converted
+            // into a list for more built-in methods to facilitate formatting.
+
             String[] entireArray = response.split("\n");
             List<String> entirelist = Arrays.asList(entireArray);
             System.out.println(entirelist);
+
+            // The int variable mcqStart marks the start of the mcq question in the list. the very next element stored after
+            // this index stores the question of the first mcq question. Same for the open-ended questions.
+
             int mcqStart = 0;
             int openStart = 0;
+
             for (String x : entireArray){
                 if (x.toUpperCase().contains("HOICE QUESTION")){
                     mcqStart = entirelist.indexOf(x);
@@ -42,6 +56,14 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
                     System.out.println(openStart);
                 }
             }
+
+            // The formatted list at this point is as following:
+            // [question_1, answerChoices, correctAnswer,
+            //  question_2, answerChoices, correctAnswer,
+            //  question_3, answerChoices, correctAnswer, ...]
+
+            // This design of list allows the following lines...:
+
             int mcq1q = mcqStart + 1;
             int mcq2q = mcq1q + 3;
             int mcq3q = mcq2q + 3;
@@ -52,6 +74,8 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
             int opq3q = opq2q + 2;
             int opq4q = opq3q + 2;
             int opq5q = opq4q + 2;
+
+            // Generating Question entities accordingly
 
             MultipleChoiceQuestion mcq1 = new MultipleChoiceQuestion(entirelist.get(mcq1q),
                     entirelist.get(mcq1q+1),
@@ -76,6 +100,9 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
             OpenEndedQuestion opq5 = new OpenEndedQuestion(entirelist.get(opq5q), entirelist.get(opq5q+1));
             List<Question<?>> qList = new ArrayList<>();
 
+            // Stores all of the questions generated into a list since the Quiz entity takes an a list attribute to store
+            // all the questions (10 of them in total).
+
             qList.add(mcq1);
             qList.add(mcq2);
             qList.add(mcq3);
@@ -87,6 +114,8 @@ public class GenerateQuizInteractor implements GenerateQuizInputBoundary {
             qList.add(opq3);
             qList.add(opq4);
             qList.add(opq5);
+
+            // Quiz entity generated
 
             Quiz quiz = new Quiz(generateQuizInputData.getTitle(), LocalDateTime.now());
             quiz.setQuestions(qList);
