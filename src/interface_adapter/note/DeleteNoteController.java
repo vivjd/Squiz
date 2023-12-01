8310206d79108a83276601a8befc831ab20312aa
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-11-30 23:17:50 -0500

    JavaDoc added for all DeleteQuiz use case related classes

--- src/interface_adapter/note/DeleteNoteController.java
+++ src/interface_adapter/note/DeleteNoteController.java
@@ -4,14 +4,27 @@ import org.bson.types.ObjectId;
 import use_case.note.delete.DeleteNoteInputBoundary;
 import use_case.note.delete.DeleteNoteInputData;
 
+/**
+ * The DeleteNoteController class is responsible for handling user input related to note deletion.
+ */
 public class DeleteNoteController {
 
     final DeleteNoteInputBoundary deleteNoteInteractor;
 
+    /**
+     * Constructs a DeleteNoteController with the specified interactor.
+     *
+     * @param deleteNoteInteractor The interactor responsible for handling the delete note use case.
+     */
     public DeleteNoteController(DeleteNoteInputBoundary deleteNoteInteractor) {
         this.deleteNoteInteractor = deleteNoteInteractor;
     }
 
+    /**
+     * Executes the note deletion based on the provided ObjectId.
+     *
+     * @param objectId The ObjectId of the note to be deleted.
+     */
     public void execute(ObjectId objectId) {
         DeleteNoteInputData deleteNoteInputData= new DeleteNoteInputData(objectId);
 
--- src/use_case/note/delete/DeleteNoteInteractor.java
+++ src/use_case/note/delete/DeleteNoteInteractor.java
@@ -2,11 +2,21 @@ package use_case.note.delete;
 
 import use_case.note.NoteDataAccessInterface;
 
+/**
+ * The DeleteNoteInteractor class is responsible for handling the business logic of deleting a note.
+ * It implements the DeleteNoteInputBoundary interface.
+ */
 public class DeleteNoteInteractor implements DeleteNoteInputBoundary {
 
     final NoteDataAccessInterface noteDataAccessObject;
     final DeleteNoteOutputBoundary deleteNoteOutputBoundary;
 
+    /**
+     * Constructs a DeleteNoteInteractor with the specified dependencies.
+     *
+     * @param noteDataAccessObject      The data access object responsible for interacting with notes.
+     * @param deleteNoteOutputBoundary The output boundary for presenting results of note deletion.
+     */
     public DeleteNoteInteractor(NoteDataAccessInterface noteDataAccessObject,
                                 DeleteNoteOutputBoundary deleteNoteOutputBoundary) {
         this.noteDataAccessObject = noteDataAccessObject;
@@ -14,6 +24,11 @@ public class DeleteNoteInteractor implements DeleteNoteInputBoundary {
     }
 
 
+    /**
+     * Executes the process of deleting a note based on the input data.
+     *
+     * @param deleteNoteInputData The input data containing the ObjectId of the note to be deleted.
+     */
     @Override
     public void execute(DeleteNoteInputData deleteNoteInputData) {
         try {
