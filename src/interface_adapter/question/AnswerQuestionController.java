dbac6dfd7ae7c3877dff47f5a7e69078f7f8f955
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-12-02 11:34:41 -0500

    answer qn controller javadocs

--- src/interface_adapter/question/AnswerQuestionController.java
+++ src/interface_adapter/question/AnswerQuestionController.java
@@ -4,14 +4,32 @@ import entity.Question;
 import use_case.question.AnswerQuestionInputBoundary;
 import use_case.question.AnswerQuestionInputData;
 
+/**
+ * Controller class responsible for handling user interactions related to answering a question.
+ */
 public class AnswerQuestionController {
+
+    /**
+     * The interactor responsible for processing question-answering operations.
+     */
     final AnswerQuestionInputBoundary answerQuestionInteractor;
 
+    /**
+     * Constructs a new AnswerQuestionController object with the specified interactor.
+     *
+     * @param answerQuestionInteractor The interactor responsible for processing question-answering operations.
+     */
     public AnswerQuestionController(AnswerQuestionInputBoundary answerQuestionInteractor) {
         this.answerQuestionInteractor = answerQuestionInteractor;
     }
 
-    public void execute(String answer, Question<?> question){
+    /**
+     * Executes the question-answering process with the specified answer and question.
+     *
+     * @param answer   The user's answer to the question.
+     * @param question The question being answered.
+     */
+    public void execute(String answer, Question<?> question) {
         AnswerQuestionInputData answerQuestionInputData = new AnswerQuestionInputData(answer, question);
         answerQuestionInteractor.execute(answerQuestionInputData);
     }
--- src/interface_adapter/question/AnswerQuestionState.java
+++ src/interface_adapter/question/AnswerQuestionState.java
@@ -2,41 +2,115 @@ package interface_adapter.question;
 
 import entity.Question;
 
+/**
+ * Represents the state of answering a question, including information about the question itself,
+ * the user's answer, feedback, and question name.
+ */
 public class AnswerQuestionState {
 
+    /**
+     * The actual question object.
+     */
     private Question<?> question;
+
+    /**
+     * The name or text representation of the question.
+     */
     private String questionName;
+
+    /**
+     * The user's answer to the question.
+     */
     private String userAnswer;
+
+    /**
+     * The feedback related to the user's answer.
+     */
     private String feedback;
 
-    public AnswerQuestionState(){
+    /**
+     * Default constructor for creating an empty AnswerQuestionState.
+     */
+    public AnswerQuestionState() {}
 
-    }
-
+    /**
+     * Sets the name or text representation of the question.
+     *
+     * @param questionName The name or text representation of the question.
+     */
     public void setQuestionName(String questionName) {
         this.questionName = questionName;
     }
 
+    /**
+     * Sets the user's answer to the question.
+     *
+     * @param userAnswer The user's answer to the question.
+     */
     public void setUserAnswer(String userAnswer) {
         this.userAnswer = userAnswer;
     }
 
+    /**
+     * Sets the feedback related to the user's answer.
+     *
+     * @param feedback The feedback related to the user's answer.
+     */
     public void setFeedback(String feedback) {
         this.feedback = feedback;
     }
 
+    /**
+     * Gets the name or text representation of the question.
+     *
+     * @return The name or text representation of the question.
+     */
     public String getQuestionName() {
         return questionName;
     }
 
+    /**
+     * Gets the user's answer to the question.
+     *
+     * @return The user's answer to the question.
+     */
     public String getUserAnswer() {
         return userAnswer;
     }
 
+    /**
+     * Gets the feedback related to the user's answer.
+     *
+     * @return The feedback related to the user's answer.
+     */
     public String getFeedback() {
         return feedback;
     }
 
+    /**
+     * Gets the actual question object.
+     *
+     * @return The actual question object.
+     */
+    public Question<?> getQuestion() {
+        return question;
+    }
+
+    /**
+     * Sets the actual question object and updates the question name based on the question.
+     *
+     * @param question The actual question object.
+     */
+    public void setQuestion(Question<?> question) {
+        this.question = question;
+        this.questionName = question.getQuestion();
+    }
+
+    /**
+     * Returns a string representation of the AnswerQuestionState.
+     *
+     * @return A string representation of the AnswerQuestionState.
+     */
     @Override
     public String toString() {
         return "AnswerQuestionState{" +
@@ -44,14 +118,5 @@ public class AnswerQuestionState {
                 ", userAnswer='" + userAnswer + '\'' +
                 ", feedback='" + feedback + '\'' +
                 '}';
-    }
-
-    public Question<?> getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question<?> question) {
-        this.question = question;
-        this.questionName = question.getQuestion();
     }
 }
