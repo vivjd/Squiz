cd46ce5c2ac3442d090d38595b5436043cc40e13
Vivian <118199397+vivjd@users.noreply.github.com>
2023-12-02 00:43:13 -0500

    DisplayNotes tests

---
+++ test/interface_adapter/note/delete/DeleteNoteControllerTest.java
@@ -0,0 +1,33 @@
+package interface_adapter.note.delete;
+
+import interface_adapter.note.DeleteNoteController;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import use_case.note.delete.DeleteNoteInputBoundary;
+
+import static org.mockito.Mockito.*;
+
+public class DeleteNoteControllerTest {
+    @Mock
+    private DeleteNoteInputBoundary deleteNoteInteractor;
+
+    private DeleteNoteController deleteNoteController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        deleteNoteController = new DeleteNoteController(deleteNoteInteractor);
+    }
+
+    @Test
+    public void testExecute() {
+        ObjectId noteId = new ObjectId("656583e8d05ae3b112ed77c1");
+
+        deleteNoteController.execute(noteId);
+
+        verify(deleteNoteInteractor).execute(argThat(argument -> argument.getObjectId().equals(noteId)));
+    }
+}
--- test/interface_adapter/note/delete/DeleteNotePresenterTest.java
+++ test/interface_adapter/note/delete/DeleteNotePresenterTest.java
@@ -1,4 +1,4 @@
-package interface_adapter.note;
+package interface_adapter.note.delete;
 
 import org.junit.Test;
 
