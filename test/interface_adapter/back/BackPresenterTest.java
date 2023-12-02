3b6761232344f7abaf71d8160e663c7966f66803
emmshen <emmy12821@gmail.com>
2023-12-02 11:53:56 -0500

    test cases for back use case

--- test/interface_adapter/back/BackPresenterTest.java
+++ test/interface_adapter/back/BackPresenterTest.java
@@ -1,6 +1,33 @@
 package interface_adapter.back;
 
+import interface_adapter.ViewManagerModel;
 import interface_adapter.back.BackPresenter;
+import interface_adapter.note.NoteViewModel;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class BackPresenterTest {
+    private ViewManagerModel viewManagerModel;
+    private NoteViewModel noteViewModel;
+    private BackPresenter backPresenter;
+
+    @Before
+    public void create_presenter() {
+        viewManagerModel = mock(ViewManagerModel.class);
+        noteViewModel = mock(NoteViewModel.class);
+        backPresenter = new BackPresenter(noteViewModel, viewManagerModel);
+    }
+
+    @Test
+    public void testSuccess() {
+        backPresenter.prepareSuccessView();
+    }
+
+    @Test
+    public void testFailed() {
+        backPresenter.prepareFailView();
+    }
 }
