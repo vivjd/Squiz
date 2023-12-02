a295a87b5064621c3027923f97b8e0b15c81370c
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-12-02 15:01:36 -0500

    SaveNoteOutputData Test Done

---
+++ test/use_case/saveNote/SaveNoteOutputDataTest.java
@@ -0,0 +1,31 @@
+package use_case.saveNote;
+import org.junit.jupiter.api.Test;
+import use_case.note.save.SaveNoteOutputData;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaveNoteOutputDataTest {
+    @Test
+    void testGetTitle() {
+        // Arrange
+        SaveNoteOutputData outputData = new SaveNoteOutputData("Test Title", "Test Input Text", false);
+        String title = outputData.getTitle();
+        assertEquals("Test Title", title);
+    }
+
+    @Test
+    void testGetInputText() {
+        // Arrange
+        SaveNoteOutputData outputData = new SaveNoteOutputData("Test Title", "Test Input Text", false);
+        String inputText = outputData.getInputText();
+        assertEquals("Test Input Text", inputText);
+    }
+
+    @Test
+    void testSetInputText() {
+        // Arrange
+        SaveNoteOutputData outputData = new SaveNoteOutputData("Test Title", "Test Input Text", false);
+        outputData.setInputText("Updated Input Text");
+        assertEquals("Updated Input Text", outputData.getInputText());
+    }
+}
