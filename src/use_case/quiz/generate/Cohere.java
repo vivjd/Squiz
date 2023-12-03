949bf3870879c8f44828af15664c3d567468e3fa
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-12-02 20:24:17 -0500

    one more dummy for Strategy Design pattern

--- src/use_case/quiz/generate/Cohere.java
+++ src/use_case/quiz/generate/Cohere.java
@@ -1,2 +1,14 @@
-package use_case.quiz.generate;public class Cohere {
+package use_case.quiz.generate;
+
+public class Cohere implements TextGeneratorEndpoints{
+
+    @Override
+    public String getUserInput() throws Exception {
+        return null;
+    }
+
+    @Override
+    public String getChatGPTResponse(String string) throws Exception {
+        return null;
+    }
 }
